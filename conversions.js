ec68b716fd45aa2753e875feb72c2c47bb4c0e3e
Saskian <bluebottel@gmail.com>
2021-08-19 08:55:54 +0200

    Better coordinate splitting
    More resistant method of extracting coordinate numbers

--- conversions.js
+++ conversions.js
@@ -5,15 +5,13 @@ const DEGREE_SYMBOL = '°'
 // 59.420894, 18.374119 => 59°25'15.2"N 18°22'26.8"E
 function wgs84decToWgs84(wgs84decCoord) {
 
-  // also remove the space after the comma
-  const [ wgs84decCoordLat, wgs84decCoordLon ] = wgs84decCoord.split(',')
-	.map(ele => parseFloat(ele.trim()))
+  const [ wgs84decCoordLat, wgs84decCoordLon ] = extractNumbers(wgs84decCoord)
 
   const convertedCoords = [ wgs84decCoordLat, wgs84decCoordLon ]
     .map(partialCoord => {
-      const wgs84degrees = partialCoord.toFixed()
-      let remainder = partialCoord - wgs84degrees
-      const wgs84minutes = (remainder * 60).toFixed()
+      const wgs84degrees = Math.trunc(parseFloat(partialCoord))
+      let remainder = parseFloat(partialCoord) - parseFloat(wgs84degrees)
+      const wgs84minutes = Math.trunc(remainder * 60)
 
       // everything after the decimal place
       remainder = (remainder * 60) % 1
@@ -21,7 +19,7 @@ function wgs84decToWgs84(wgs84decCoord) {
       const wgs84seconds = remainder * 60
 
       return wgs84degrees + DEGREE_SYMBOL + wgs84minutes
-	   + "'" + wgs84seconds + '\"'
+	   + "'" + wgs84seconds + '"'
     })
 
   return convertedCoords[0] + 'N ' + convertedCoords[1] + 'E'  
@@ -31,12 +29,11 @@ function wgs84decToWgs84(wgs84decCoord) {
 function wgs84decToWgs84ddm(wgs84decCoord) {
 
   // also remove the space after the comma
-  const [ wgs84decCoordLat, wgs84decCoordLon ] = wgs84decCoord.split(',')
-	.map(ele => parseFloat(ele.trim()))
+  const [ wgs84decCoordLat, wgs84decCoordLon ] = extractNumbers(wgs84decCoord)
 
   const convertedCoords = [ wgs84decCoordLat, wgs84decCoordLon ]
 	.map(partialCoord => {
-	  const wgs84degrees = partialCoord.toFixed()
+	  const wgs84degrees = Math.trunc(partialCoord)
 	  const wgs84minutes = (partialCoord - wgs84degrees) * 60
 
 	  return wgs84degrees + DEGREE_SYMBOL + wgs84minutes
@@ -49,48 +46,33 @@ function wgs84decToWgs84ddm(wgs84decCoord) {
 // 59°25.254'N 18°22.447'E => 59.420894, 18.374119
 function wgs84ddmToWgs84dec(wgs84ddmCoord) {
 
-  const [ wgs84ddmLat, wgs84ddmLon ] = wgs84ddmCoord.split(' ')
+  const [ wgs84ddmLatDegrees, wgs84ddmLatMinutes,
+	  wgs84ddmLonDegrees, wgs84ddmLonMinutes ] = extractNumbers(wgs84ddmCoord)
 
-  let cleanWgs84ddmLat = wgs84ddmLat.replaceAll(/['NE]/g, '')
-  cleanWgs84ddmLat = cleanWgs84ddmLat.replaceAll('°', ' ')
+  const lat = parseFloat(wgs84ddmLatDegrees)
+	    + parseFloat(wgs84ddmLatMinutes) / 60
 
-  let cleanWgs84ddmLon = wgs84ddmLon.replaceAll(/['NE]/g, '')
-  cleanWgs84ddmLon = cleanWgs84ddmLon.replaceAll('°', ' ')
-
-
-  // separate degrees and minutes
-  cleanWgs84ddmLon = cleanWgs84ddmLon.split(' ')
-  cleanWgs84ddmLat = cleanWgs84ddmLat.split(' ')
-
-  
-  const lat = parseFloat(cleanWgs84ddmLat[0])
-	    + parseFloat(cleanWgs84ddmLat[1]) / 60
-
-  const lon = parseFloat(cleanWgs84ddmLon[0])
-	    + cleanWgs84ddmLon[1] / 60
+  const lon = parseFloat(wgs84ddmLonDegrees)
+	    + parseFloat(wgs84ddmLonMinutes) / 60
 
   return `${lat}, ${lon}`
 }
 
 // 59°25'15.2"N 18°22'26.8"E => 59.420894, 18.374119
 function wgs84toWgs84dec(wgs84coord) {
-  const [ wgs84lat, wgs84lon ] = wgs84coord.split(' ')
-  const [ cleanWgs84lat, cleanWgs84lon ] =
-	[ wgs84lat, wgs84lon ].map(dirtyCoord => {
-	  dirtyCoord = dirtyCoord.replaceAll(/['°]/g, ' ')
-	  dirtyCoord = dirtyCoord.replaceAll(/["NE]/g, '')
-	  return dirtyCoord
-	})
+  const [ wgs84latDegrees, wgs84latMinutes, wgs84latSeconds,
+	  wgs84lonDegrees, wgs84lonMinutes, wgs84lonSeconds ] = extractNumbers(wgs84coord)
 
-  const finishedWgs84lat = cleanWgs84lat.split(' ')
-	.reduce((accumulator, ele, i) =>
-		parseFloat(accumulator) + ele / 60**i)
+  const wgs84lat = parseFloat(wgs84latDegrees)
+		 + parseFloat(wgs84latMinutes) / 60
+		 + parseFloat(wgs84latSeconds) / ( 60 * 60 )
 
-   const finishedWgs84lon = cleanWgs84lon.split(' ')
-	.reduce((accumulator, ele, i) =>
-		parseFloat(accumulator) + ele / 60**i)
+  const wgs84lon = parseFloat(wgs84lonDegrees)
+		 + parseFloat(wgs84lonMinutes) / 60
+		 + parseFloat(wgs84lonSeconds) / ( 60 * 60 )
+
   
-  return `${finishedWgs84lat}, ${finishedWgs84lon}`
+  return `${wgs84lat}, ${wgs84lon}`
 }
 
 function wgs84toWgs84ddm(wgs84coord) {
@@ -126,7 +108,7 @@ function getConversionFunction({ sourceFormat, targetFormat }) {
       break;
 
     case 'wgs84dec':
-      if (targetFormat === 'wgs84') func = wgs84decToWgs84dec
+      if (targetFormat === 'wgs84') func = wgs84decToWgs84
       if (targetFormat === 'wgs84ddm') func = wgs84decToWgs84ddm
       if (targetFormat === 'wgs84dec') func = false
       break;
@@ -157,6 +139,10 @@ function joinLatLon({ format, lat, lon }) {
   }
 
   return result
+}
+
+function extractNumbers(coord) {
+  return coord.match(/[0-9.]+/g)
 }
 
 function hasHeaders(data) {
